fb8f161b6f307eafa0f1fe44846abf799f08644b
Nirmal <nirmalr2512@gmail.com>
2023-02-25 16:19:03 +0530

    🐛 bug fixed

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,7 +4,7 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const organizationName = "go-niom";
-const projectName = "niom";
+const projectName = "niom-docs";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
